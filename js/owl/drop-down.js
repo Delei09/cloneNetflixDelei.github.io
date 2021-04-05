980f611b2f1768e8b4f39175a4745f9461e3093f
Delei09 <vanderlei_09@outlook.com>
2021-04-05 10:31:19 -0300

    First

---
+++ js/owl/drop-down.js
@@ -0,0 +1,5 @@
+let botao = document.querySelector(".drop-down-header-menu");
+let navegar = document.querySelector(".navegar");
+navegar.addEventListener("click" , function(e){
+   botao.classList.toggle("show");
+})
